95b51efcb73bb104db2c4819d01baa9d1ee4397f
Adam Weissman <adam@comedytragedyepic.com>
2019-09-14 15:01:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,11 @@
 def my_each(collection) # put argument(s) here
   i = 0
+  
   while i < collection.length
-  yield
-  i+= 1
-end
+    yield
+    i+= 1
+  end
+  
 end
 
 my_each(collection) do |i|
